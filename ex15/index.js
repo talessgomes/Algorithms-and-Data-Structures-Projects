c186514b02c19107850c1a34ae23bba7e196a079
talessgomes <tales.santos.gomes@outlook.com>
2023-01-17 11:21:24 -0300

    finalizado-exercicio-15

---
+++ ex15/index.js
@@ -0,0 +1,9 @@
+function dropElements(arr, func) {
+    while (arr.length > 0 && !func(arr[0])) {
+        arr.shift();
+    }
+
+    return arr;
+}
+
+console.log(dropElements([1, 2, 3, 4], function (n) { return n >= 3; })); //[ 3, 4 ]
